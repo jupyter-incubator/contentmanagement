6222fde807743e38d9133c4c50cda27730a31398
Peter Parente <pparent@us.ibm.com>
2015-11-21 23:11:00 -0500

    Add load_ipython_extension to JS mains
    Silence error about undefined module on load
    
    (c) Copyright IBM Corp. 2015

--- urth_cms_js/editor/main.js
+++ urth_cms_js/editor/main.js
@@ -8,7 +8,11 @@ define([
     '../common/dnd_upload',
     './search'
 ], function(IPython, utils, upload) {
-    // Use the current file directory as the upload path
-    var segs = utils.url_path_split(IPython.editor.file_path);
-    upload.set_upload_path(segs[0]);
+    return {
+        load_ipython_extension: function() {
+            // Use the current file directory as the upload path
+            var segs = utils.url_path_split(IPython.editor.file_path);
+            upload.set_upload_path(segs[0]);
+        }
+    };
 });
